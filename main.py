2847d664e79b59f5ee6d8a1edbbc23f495c46ba2
JurisDataNerd <158026102+JurisDataNerd@users.noreply.github.com>
2024-11-05 06:38:22 +0000

    commit edit

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("🎈 Fauzan Arisanto Personal Portfolio!")
+st.write(
+    "Hello! I'm [Your Name], a [Your Profession]. Here's a showcase of my work, skills, and contact info.)."
+)
